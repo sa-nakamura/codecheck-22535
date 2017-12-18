629237dcc729b48f4bd6440026d89aa0b4ee5e2e
sa-nakamura <sa-nakamura@voltage@co.jp>
2017-12-18 17:11:16 +0900

    no message

--- sql/createJohn.sql
+++ sql/createJohn.sql
@@ -1,3 +1,5 @@
 -- table: users
 -- Create John here!
 
+INSERT INTO users (name,password,gender,created_at,deleted_at)
+VALUES  ("John Legendary","75D2DB8E-45E5-08D5-6669-BBA06F615472",1,"2017-04-01 08:13:56",NULL);
