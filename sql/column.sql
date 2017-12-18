aa7a2a4c49774ee364d43001e0c9cf1be92d2eb7
sa-nakamura <sa-nakamura@voltage@co.jp>
2017-12-18 17:08:35 +0900

    no message

--- sql/column.sql
+++ sql/column.sql
@@ -2,6 +2,6 @@
 -- select non deleted users
 -- column "password" should not be contained
 
-SELECT id, name, gender, created_at, deleted_at
+SELECT name, gender, created_at, deleted_at
  FROM users
  WHERE deleted_at = NUll;
