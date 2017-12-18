f251bb84296eaade3d24784fa7a616981a370f10
sa-nakamura <sa-nakamura@voltage@co.jp>
2017-12-18 17:23:23 +0900

    no message

--- sql/deleteDuplicates.sql
+++ sql/deleteDuplicates.sql
@@ -1,3 +1,14 @@
 -- table: users
 -- Delete the older entry (larger id) of any duplicated entries.
 
+DELETE *
+FROM
+    users
+WHERE id < (
+ SELECT max(id)
+ FROM
+    users
+ GROUP BY
+    name
+ HAVING COUNT(name) > 1 ;
+)
